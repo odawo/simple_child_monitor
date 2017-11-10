c299bb401acda8f844c8cedbeb4cf021dc725bad
odawo <vanessaodawo@gmail.com>
2017-11-10 13:41:29 +0300

    new commit

--- app/src/main/java/com/example/vanessa/childdevelopmentapp/ZeroActivity.java
+++ app/src/main/java/com/example/vanessa/childdevelopmentapp/ZeroActivity.java
@@ -3,81 +3,123 @@ package com.example.vanessa.childdevelopmentapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.example.vanessa.childdevelopmentapp.classes.Question;
-import com.example.vanessa.childdevelopmentapp.classes.QuestionBank;
+import com.example.vanessa.childdevelopmentapp.model.Question;
+import com.example.vanessa.childdevelopmentapp.db.MyDatabaseHelper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ZeroActivity extends AppCompatActivity {
 
-    private QuestionBank mQnLib = new QuestionBank();
+    private List<Question> questionsList;
+    private Question currentQuestion;
 
-    Button btnchoice1, btnchoice2;
-    TextView questionview, scoreview;
+    private TextView txtQuestion,txtNoOfQns;
+    private RadioButton btnchoice1, btnchoice2;
+    private ImageButton btnNext;
 
-    private String mAnswer;
-    private int mScore = 0;
-    private int mQuestionNumber = 0;
+    private int obtainedScore=0;
+    private int questionId=0;
+
+    private int answeredQsNo=0;
+
+    ArrayList<String> myAnsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_zero);
 
-        btnchoice1 = (Button)findViewById(R.id.btnchoice1);
-        btnchoice2 = (Button)findViewById(R.id.btnchoice2);
-        questionview = (TextView) findViewById(R.id.question);
-        scoreview = (TextView) findViewById(R.id.score);
+//        initialize view
+        init();
 
-        mQnLib.initQuestions(getApplicationContext());
-        updateQuestion();
-//        show current score. The score helps gauge the child's devt
-        updateScore(mScore);
+//        initialize database
+        final MyDatabaseHelper myDatabaseHelper = new MyDatabaseHelper(this);
+        questionsList = myDatabaseHelper.getAllQuestionsList();
+        currentQuestion = questionsList.get(questionId);
+
+//        set Questions
+        setQuestionsView();
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RadioGroup rgroup = (RadioGroup)findViewById(R.id.radiogroup);
+                RadioButton answer = (RadioButton)findViewById(rgroup.getCheckedRadioButtonId());
+
+                Log.e("Answer ID", "Selected Position Value - " +rgroup.getCheckedRadioButtonId());
+
+                if(answer!=null){
+                    Log.e("Answer", currentQuestion.getAnswer() + " -- " + answer.getText());
+                    //Add answer to the list
+                    myAnsList.add(""+ answer.getText());
+
+                    if(currentQuestion.getAnswer().equals(answer.getText())){
+                        obtainedScore++;
+                        Log.e("comments", "Correct Answer");
+                        Log.d("score", "Obtained score " + obtainedScore);
+                    }else{
+                        Log.e("comments", "Wrong Answer");
+                    }
+                    if(questionId<myDatabaseHelper.rowCount()){
+                        currentQuestion=questionsList.get(questionId);
+                        setQuestionsView();
+                    }else{
+                        Intent intent = new Intent(ZeroActivity.this, FinalDisplay.class);
+
+                        Bundle b = new Bundle();
+                        b.putInt("score", obtainedScore);
+                        b.putInt("totalQs", questionsList.size());
+                        b.putStringArrayList("myAnsList", myAnsList);
+                        intent.putExtras(b);
+                        startActivity(intent);
+                        finish();
+
+                    }
+
+                } else {
+                    Log.e("comments", "No Answer");
+                }
+
+//                clear checked item
+                rgroup.clearCheck();
+            }
+        });
     }
 
-    private void updateQuestion() {
 
-//        check if not outside array bounds
+    private void init() {
 
-        if (mQuestionNumber<mQnLib.getLength()) {
-            //set text for new qn
-            questionview.setText(mQnLib.getQuestion(mQuestionNumber));
-            btnchoice1.setText(mQnLib.getChoice(mQuestionNumber, 1));
-            btnchoice2.setText(mQnLib.getChoice(mQuestionNumber, 2));
-            mAnswer = mQnLib.getCorrectAnswer(mQuestionNumber);
-            mQuestionNumber++;
+        btnchoice1 = (RadioButton) findViewById(R.id.btnchoice1);
+        btnchoice2 = (RadioButton) findViewById(R.id.btnchoice2);
+        txtQuestion = (TextView) findViewById(R.id.question);
+        txtNoOfQns = (TextView) findViewById(R.id.questionno);
 
-        } else {
-            Toast.makeText(ZeroActivity.this, "end of questionnaire. wait as results load", Toast.LENGTH_LONG).show();
-            Intent intent = new Intent(ZeroActivity.this, FinalDisplay.class);
-            //pass score data to FinalDisplay activity
-            intent.putExtra("score", mScore);
-            startActivity(intent);
-        }
+        btnNext = (ImageButton) findViewById(R.id.btnnext);
 
+        myAnsList = new ArrayList<String>();
     }
 
-    //show current total
-    private void updateScore(int point) {
-        scoreview.setText("" +mScore+"/" +mQnLib.getLength());
+
+    private void setQuestionsView() {
+
+        btnchoice1.setChecked(false);
+        btnchoice2.setChecked(false);
+
+        answeredQsNo = questionId+1;
+        txtNoOfQns.setText("Questions "+answeredQsNo+" of "+questionsList.size());
+        txtQuestion.setText(currentQuestion.getQuestion());
+        btnchoice1.setText(currentQuestion.getChoiceyes());
+        btnchoice2.setText(currentQuestion.getChoiceno());
+
+        questionId++;
     }
 
-    public void onClick(View view) {
-        //all logic for all answers button in one method
-        Button answer = (Button) view;
-        
-        if (answer.getText().equals(mAnswer)) {
-            //score increase once answer is correct
-            mScore = mScore + 1;
-        }
-
-//      show current score total
-        updateScore(mScore);
-
-//        moves user to next screen once qn is answered
-        updateQuestion();
-    }
 }
